f98d0c23f3599e58cb82dc382831a7f05898d7b1
egorb <egorb@MI_G_LAPTOP>
2022-10-04 23:31:56 +0300

    Добавьте файлы проекта.

---
+++ GeographObject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+abstract class GeographicalObject 
+{
+    private decimal x;
+    private decimal y;
+    private string name;
+    private string description;
+    public virtual void GetInfo() 
+    {
+    }
+}
+class River : GeographicalObject 
+{
+    private double speed;
+    private double length;
+}
+
+class Mountain : GeographicalObject 
+{
+    private double peak;
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,29 @@
+﻿interface IGeographicalObject
+{
+    decimal x { get; }
+    decimal y { get; }
+    string name { get; }
+    string description { get; }
+    virtual void GetInfo()
+    {
+    }
+}
+
+class River : IGeographicalObject
+{
+    public decimal x { get;}
+    public decimal y { get;}
+    public string name { get;}
+    public string description { get;}
+    public double speed { get; }
+    public double length { get; }
+}
+
+class Mountain : IGeographicalObject
+{
+    public decimal x { get; }
+    public decimal y { get; }
+    public string name { get; }
+    public string description { get; }
+    public double peak { get; }
+}
