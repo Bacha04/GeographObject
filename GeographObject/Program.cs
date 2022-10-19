b814d8ba246600f0a3198e3dfd58d88b168204a6
Bacha04 <114564813+Bacha04@users.noreply.github.com>
2022-10-19 09:36:52 +0300

    Update Program.cs

--- GeographObject/Program.cs
+++ GeographObject/Program.cs
@@ -1,22 +1,38 @@
-﻿using System;
+using System;
+using System.Runtime.CompilerServices;
 
+Mountain mountain = new Mountain();
+mountain.description = "The highest mountain in the world ";
+mountain.peak = 2003;
+mountain.GetInfo();
 abstract class GeographicalObject 
 {
-    private decimal x;
-    private decimal y;
-    private string name;
-    private string description;
+    public decimal x;
+    public decimal y;
+    public string name;
+    public string description;
     public virtual void GetInfo() 
     {
+        Console.WriteLine(description); 
     }
 }
-class River : GeographicalObject 
+class River : GeographicalObject
 {
     private double speed;
     private double length;
+    public override void GetInfo()
+        {
+        base.GetInfo();
+        Console.WriteLine(length);    
+        }
 }
 
 class Mountain : GeographicalObject 
 {
-    private double peak;
+    public double peak;
+    public override void GetInfo()
+    {
+        base.GetInfo();
+        Console.WriteLine(peak);
+    }
 }
