52fcacc0bf1f9fe90cacc39c3fb6745029bc922b
Bacha04 <114564813+Bacha04@users.noreply.github.com>
2022-10-19 09:38:09 +0300

    Update Program.cs

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -1,29 +1,40 @@
-﻿interface IGeographicalObject
+River river = new River();
+river.name = "Dnipro";
+
+interface IGeographicalObject
 {
     decimal x { get; }
     decimal y { get; }
     string name { get; }
     string description { get; }
-    virtual void GetInfo()
-    {
-    }
+    public void GetInfo();
+    
 }
+
 
 class River : IGeographicalObject
 {
-    public decimal x { get;}
-    public decimal y { get;}
-    public string name { get;}
-    public string description { get;}
-    public double speed { get; }
-    public double length { get; }
+    public decimal x { get; set; }
+    public decimal y { get; set; }
+    public string name { get; set; }
+    public string description { get; set; }
+    public double speed { get; set; }
+    public double length { get; set; }
+    public void GetInfo() 
+    {
+        Console.WriteLine(description);
+    }
 }
 
 class Mountain : IGeographicalObject
 {
-    public decimal x { get; }
-    public decimal y { get; }
-    public string name { get; }
-    public string description { get; }
-    public double peak { get; }
+    public decimal x { get; set; }
+    public decimal y { get; set; }
+    public string name { get; set; }
+    public string description { get; set; }
+    public double peak { get; set; }
+    public void GetInfo()
+    {
+        Console.WriteLine(name);
+    }
 }
